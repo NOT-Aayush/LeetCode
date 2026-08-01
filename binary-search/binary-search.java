eecd31b37cb9b06c4096115fa20f0bc4ba0a70eb
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-01 21:37:23 +0530

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -2,15 +2,18 @@ class Solution {
     public int search(int[] nums, int target) {
         int i=0;
         int j=nums.length-1;
+
         while( i<=j ){
-            if ( nums[i] == target){
-                return i;
+            int mid = i + (j-i)/2;
+            if ( nums[mid] == target){
+                return mid;
             }
-            if (nums[j] == target){
-                return j;
+            else if (nums[mid] > target){
+                i++;
             }
-            i++;
-            j--;
+            else{
+                j++;
+            }
         }
         return -1;
     }
