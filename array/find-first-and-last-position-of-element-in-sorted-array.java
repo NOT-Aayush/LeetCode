608f6c4cf1e9d92fb4d1ab20ffaf49fe504abca8
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-02 13:03:38 +0530

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-first-and-last-position-of-element-in-sorted-array.java
+++ array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        int[] len = new int[]{-1,-1};
+    static int bs( int[] nums, int target, int l, int r, boolean swtch){
+        int ans=-1;
         while (l <= r){
             int mid = l + (r-l)/2;
-            if (nums[mid] == target && (mid==nums.length-1 || nums[mid+1] != target)){
-                len[1]=mid;
+            if (nums[mid] == target && !swtch){
+                ans=mid;
                 r = mid-1;
             }
-            else if (nums[mid]==target && (mid==0 || nums[mid-1] != target)){
-                len[0]=mid;
+            else if (nums[mid] == target && swtch){
+                ans=mid;
                 l = mid+1;
             }
-
             else if(nums[mid]>target){
                 r = mid-1;
             }
@@ -22,6 +19,16 @@ class Solution {
                 l= mid+1;
             }
         }
+        return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int[] len = new int[]{-1,-1};
+        boolean swtch = false;
+        len[0] = bs(nums,target,l,r,swtch);
+        swtch =true;
+        len[1] = bs(nums,target,l,r,swtch);
         return len;
     }
 }
