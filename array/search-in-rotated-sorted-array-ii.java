1e216825a038fe368d99e40be43894635b4116f1
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-04 20:13:02 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

---
+++ array/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    static boolean bs(int[] nums, int target, int l, int r){
+        while (l<=r){
+            int mid = l + (r-l)/2;
+            if (nums[mid]==target){
+                return true;
+            }
+            else if (nums[mid]<target){
+                l = mid+1;
+            }
+            else{
+                r = mid+1;
+            }
+        }
+        return false;
+    }
+    public boolean search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        return bs(nums,target,l,r);
+    }
+}
