e8e70fdd48d3263388d685c872334653ff2cbab8
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-05 15:03:15 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array.java
+++ binary-search/search-in-rotated-sorted-array.java
@@ -5,19 +5,21 @@ class Solution {
             if (nums[mid]==target){
                 return mid;
             }
-            else if (nums[mid] >= nums[l]){
-                if (nums[l] > target || nums[mid] < target){
-                     l = mid+1;
+            else if ( nums[l] <= nums[mid]){
+                if (nums[l] <= target && nums[mid] >= target){
+                     r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            else{
+                if ( nums[mid] <=target && nums[r]>=target){
+                    l = mid+1;
                 }
                 else{
                     r = mid-1;
                 }
-            }
-            else if (nums[mid]<target){
-                l = mid+1;
-            }
-            else{
-                r = mid-1;
             }
         }
         return -1;
