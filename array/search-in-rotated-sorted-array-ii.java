4b82d65e7abf71e39570838f71e042a2d240346a
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-05 15:11:48 +0530

    LC-search-in-rotated-sorted-array-ii [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array-ii.java
+++ array/search-in-rotated-sorted-array-ii.java
@@ -5,11 +5,25 @@ class Solution {
             if (nums[mid]==target){
                 return true;
             }
-            else if (nums[mid]<target){
-                l = mid+1;
+            else if (nums[l] == nums[mid] && nums[mid] == nums[r]){
+                l++;
+                r--;
             }
-            else{
-                r = mid-1;
+            else if (nums[mid] >= nums[l]){
+                if (nums[l] <= target && nums[mid] > target){
+                     r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            else {
+                if (nums[mid]<=target && nums[r]>=target){
+                    l = mid+1;
+                }
+                else{
+                    r = mid-1;
+                }
             }
         }
         return false;
