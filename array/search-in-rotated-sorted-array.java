7d31de7dca30c887078b71a6ceae2e3baedbdc33
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-05 14:50:56 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.java
+++ array/search-in-rotated-sorted-array.java
@@ -1,50 +1,30 @@
 class Solution {
-    static int findPvt(int[] nums,int l,int r){
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if (mid<nums.length-1 && nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            else if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                return mid - 1;
-            }
-            else if (nums[mid]<nums[l]){
-                r = mid-1;
-            }
-
-            else{
-                l = mid+1;
-            }
-        }
-        return -1;
-    }
-    static int bs(int[] nums,int target,int l,int r){
+    static int bs(int[] nums, int target, int l, int r){
         while (l<=r){
             int mid = l + (r-l)/2;
             if (nums[mid]==target){
                 return mid;
             }
-            else if (nums[mid]>target){
-                 r =mid-1;
+            else if (nums[mid] >= nums[l]){
+                if (nums[l] > target || nums[mid] < target){
+                     l = mid+1;
+                }
+                else{
+                    r = mid-1;
+                }
+            }
+            else if (nums[mid]<target){
+                l = mid+1;
             }
             else{
-                l = mid+1;
+                r = mid-1;
             }
         }
         return -1;
     }
     public int search(int[] nums, int target) {
         int l=0;
-        int r = nums.length-1;
-        int pvt = findPvt(nums,l,r);
-        if (pvt == -1) {
-            return bs(nums, target, 0, nums.length - 1);
-        }
-        int left = bs(nums,target,l,pvt);
-        int right = bs(nums,target,pvt+1,r);
-        if (left != -1){
-            return left;
-        }
-        return right;
+        int r=nums.length-1;
+        return bs(nums,target,l,r);
     }
 }
