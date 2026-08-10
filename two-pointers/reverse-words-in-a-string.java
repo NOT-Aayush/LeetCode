d0302592a0c53d9f601ddab0681ac658a35e0ad1
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-10 22:53:53 +0530

    LC-reverse-words-in-a-string [Runtime: N/A; Memory: N/A]

--- two-pointers/reverse-words-in-a-string.java
+++ two-pointers/reverse-words-in-a-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
         s = s.trim();
-        String[] sl = s.split("\\s+");
+        String[] sl = s.split(" +");
         StringBuilder ans = new StringBuilder();
         for (int i=sl.length-1;i>=0;i--){
             ans.append(sl[i]+" ");
