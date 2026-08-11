c4ab8a87f7225e735bfc33f1a6728e0d686f46f5
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-11 21:22:43 +0530

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

--- string/isomorphic-strings.java
+++ string/isomorphic-strings.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character,Character> map = new HashMap<>();
+        HashMap<Character,Character> map2 = new HashMap<>();
         for (int i=0 ; i<s.length();i++){
-            if(map.containsKey(s.charAt(i)) && !(map.get(s.charAt(i)) == t.charAt(i))){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a) && !(map.get(a) == b)){
                 return false;
             }
-            map.put(s.charAt(i),t.charAt(i));
+            else if(map2.containsKey(b) && !(map2.get(b) == a)){
+                return false;
+            }
+            map.put(a,b);
+            map2.put(b,a);
         }
         return true;
     }
