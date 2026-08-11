ae30c98fd33f63eb8ac52909cd88a587233c399a
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-11 21:04:47 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.java
+++ array/longest-common-prefix.java
@@ -1,24 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0){
-            return "";
-        }
-        String cm = strs[0];
-        String ans="";
-        for (int i =1 ; i<strs.length; i++){
-            ans = "" ;
-            int n=Math.min(strs[i].length(),cm.length());
-            for (int j=0;j<n;j++){
-                if (strs[i].charAt(j) == cm.charAt(j)){
-                    ans += strs[i].charAt(j);
-                }
-                else{
-                    break;
-                }
+        StringBuilder lcp = new StringBuilder();
+        lcp.append(strs[0]);
+        for (String i: strs){
+            int count =0;
+            while (count < i.length() && count < lcp.length() && i.charAt(count) == lcp.charAt(count)){
+                count++;
             }
-            cm=ans;
+            lcp.setLength(count);
+            if (lcp.length() == 0){
+                return "";
+            }
         }
-        return cm;
+        return lcp.toString();
     }
-        
 }
