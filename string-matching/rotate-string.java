9df50be5fb0e7cc2b811fe7dccf691766de60385
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-12 21:27:26 +0530

    LC-rotate-string [Runtime: N/A; Memory: N/A]

--- string-matching/rotate-string.java
+++ string-matching/rotate-string.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        int k=0;
-        int len =0;
-        for (int i=0; i < s.length();i++){
-            if ( s.charAt((i+k)%s.length()) == goal.charAt(i)){
-                len++;
-                if(len == goal.length()){
-                    return true;
-                }
-            }
-            else{
-                k++;
-            }
-        }
-        return false;
+        if (s.length() != goal.length()) return false;
+        return (s+s).contains(goal);
     }
 }
