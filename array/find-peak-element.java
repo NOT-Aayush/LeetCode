0906af4be4d25928f789b7776c21e0f76588624d
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-13 22:11:13 +0530

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

---
+++ array/find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left =0;
+        int right = nums.length-1;
+        while (left<right){
+            int mid = left + (right - left)/2;
+            if (mid - 1 >=0 && mid+1 <nums.length){
+                if ((nums[mid-1] <= nums[mid]) && (nums[mid] >= nums[mid+1])){
+                    return mid;
+                }
+            }
+            if(mid-1 >=0 && nums[mid-1] > nums[mid]){
+                right = mid;
+            }
+            else if (mid+1 <nums.length && nums[mid] < nums[mid+1]){
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
