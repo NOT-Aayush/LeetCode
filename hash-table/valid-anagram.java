a54357d1358fcb4bf2cf2e602ef2e4de966148e7
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-14 21:09:39 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

---
+++ hash-table/valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> map1 = new HashMap<>();
+
+        if (s.length() != t.length()) return false;
+        for (int i=0; i <s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
+            map1.put(t.charAt(i), map1.getOrDefault(t.charAt(i),0)+1);
+        }
+        return map.equals(map1);
+    }
+}
