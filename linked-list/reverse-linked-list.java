23035c8e2e363940bae826ddf3b3ff15b89f331a
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-18 21:59:27 +0530

    LC-reverse-linked-list [Runtime: 1 ms; Memory: 44.4 MB]

---
+++ linked-list/reverse-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode curr = head;
+        while (curr!=null){
+            arr.add(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        int i=arr.size()-1;
+        while (curr!=null){
+            curr.val = arr.get(i);
+            i--;
+            curr = curr.next;
+        }
+        return head;
+    }
+}
