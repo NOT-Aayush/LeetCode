f83eb19dd800e2fa6fcdd486c4ea0fb5124347af
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-18 22:23:16 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

---
+++ linked-list/linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(slow.next!=null){
+            slow = slow.next;
+            if (slow.next == fast.next){
+                return true;
+            }
+            if (fast.next.next==null){
+                return false;
+            }
+            fast = fast.next.next;
+
+        }
+        return false;
+    }
+}
