db48a99353bef110e711a643618f39a34ada186f
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-19 21:26:27 +0530

    LC-palindrome-linked-list [Runtime: N/A; Memory: N/A]

---
+++ recursion/palindrome-linked-list.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode curr = head;
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(curr!=null){
+            arr.add(curr.val);
+            curr=curr.next;
+        }
+        ArrayList rev = new ArrayList(arr);
+        Collections.reverse(rev);
+        return arr.equals(rev);
+    }
+}
