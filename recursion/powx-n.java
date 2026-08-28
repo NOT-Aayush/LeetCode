73c34a91a1b62785b83fdb84af1532a1073e7a11
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-08-28 11:46:50 +0530

    LC-powx-n [Runtime: N/A; Memory: N/A]

--- recursion/powx-n.java
+++ recursion/powx-n.java
@@ -1,15 +1,18 @@
 class Solution {
+    static double pow(double x,long nm, double ans){
+        if (nm == 0) return ans;
+        if ( nm % 2 == 1){         
+            ans*=x;
+            nm--;
+        }
+        x*=x;
+        nm/=2;
+        return pow(x,nm,ans);
+    }
     public double myPow(double x, int n) {
         long nm = Math.abs((long)n);
         double ans = 1;
-        while (nm>0){
-            if ( nm % 2 == 1){         
-                ans*=x;
-                nm--;
-            }
-            x*=x;
-            nm/=2;
-        }
+        ans = pow(x,nm,ans);
         if(n<0) ans = (double) 1.0 / ans;
         return ans;
     }
