79e3869a96d27a7cb3e200ddddc0fed27f838065
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-09-05 18:54:25 +0530

    LC-smallest-stable-index-ii [Runtime: N/A; Memory: N/A]

--- prefix-sum/smallest-stable-index-ii.java
+++ prefix-sum/smallest-stable-index-ii.java
@@ -2,14 +2,21 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
         int max = nums[0];
+        int min = nums[n-1];
+        int[] maxs = new int[n];
+        int[] mins = new int[n];
         for (int i=0;i<n;i++){
             max = Math.max(max,nums[i]);
-            int min = nums[i];
-            for (int j=i;j<n;j++){
-                min = Math.min(min,nums[j]);
-            }
-            if ( max-min <=k) return i;
+            maxs[i] = max;
         }
+        for (int i=n-1;i>0;i--){
+            min = Math.min(min,nums[i]);
+            mins[i] = min;
+        }
+        for (int i=0;i<n;i++){
+            if ((maxs[i]-mins[i]) <=k) return i;
+        }
+
         return -1;
     }
 }
