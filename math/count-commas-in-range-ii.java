06e8af28fac97a01e8077058d52be4658a1ad1d4
Aayush_Pandey <88243555+NOT-Aayush@users.noreply.github.com>
2026-09-09 21:35:12 +0530

    LC-count-commas-in-range-ii [Runtime: N/A; Memory: N/A]

--- math/count-commas-in-range-ii.java
+++ math/count-commas-in-range-ii.java
@@ -3,8 +3,13 @@ class Solution {
         String s = Long.toString(n);
         long len = (long)s.length();
         if (len <4) return 0;
+        long ans =0;
         long inc = (len-1)/3;
-        long num = (long) Math.pow(10,len-1);
-        return (n-num+1)*inc;
+        //long num = (long) Math.pow(10,len-1);
+        for (long i=1 ; i<=inc ; i++){
+            long num = (long) Math.pow(10, 3*i);
+            ans +=(n-num+1)*i;
+        }
+        return ans;
     }
 }
